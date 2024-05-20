a8e8ae92ce9959cedc55c5198d65801c3f546881
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-05-20 15:20:35 +0200

    chore: :art: configure Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        error: 'rgba(var(--color-error))',
+      },
+      textColor: {
+        heading: 'rgba(var(--color-text-heading))',
+        body: 'rgba(var(--color-text-body))',
+      },
+      borderColor: {
+        default: 'rgba(var(--color-border))',
+      },
+      backgroundColor: {
+        root: 'rgba(var(--color-bg-root))',
+        primary: 'rgba(var(--color-bg-primary))',
+        accent: 'rgba(var(--color-bg-accent))',
+        'nav-menu': 'rgba(var(--color-bg-nav-menu))',
+      },
+      fontFamily: {
+        default: ['Rubik', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
