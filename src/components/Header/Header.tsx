29c174ba8bacf1441758e3f3c3712fdada01edb5
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-05-21 12:30:25 +0200

    feat: update Header component to include a responsive navigation menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,22 @@
 import logoBookmark from '../../assets/logo-bookmark.svg'
-import hamburger from '../../assets/icon-hamburger.svg'
+import hamburgerIcon from '../../assets/icon-hamburger.svg'
+import { useState } from 'react'
+import { createPortal } from 'react-dom'
+import NavMenu from './NavMenu'
 
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
   return (
-    <header className="mx-[1.625rem] my-8 flex items-center justify-between">
-      <img
-        className="h-[1.3125rem]"
-        src={logoBookmark}
-        alt="Main site logo for Bookmark"
-      />
-      <button className="h-3" type="button" onClick={() => console.log('menu')}>
-        <img src={hamburger} alt="Icon to access the menu" />
+    <header
+      className={`mx-[1.625rem] my-8 flex items-center justify-between ${isMenuOpen ? 'hidden' : ''}`}
+    >
+      <img src={logoBookmark} alt="Main site logo for Bookmark" />
+      <button className="h-3" type="button" onClick={() => setIsMenuOpen(true)}>
+        <img src={hamburgerIcon} alt="Icon to access the menu" />
       </button>
+      {isMenuOpen &&
+        createPortal(<NavMenu setIsMenuOpen={setIsMenuOpen} />, document.body)}
     </header>
   )
 }
---
+++ src/components/Header/NavMenu.tsx
@@ -0,0 +1,53 @@
+import logoBookmarkLight from '../../assets/logo-bookmark-light.svg'
+import closeIcon from '../../assets/icon-close.svg'
+import twitterIcon from '../../assets/icon-twitter.svg'
+import facebookIcon from '../../assets/icon-facebook.svg'
+
+function NavMenu({ setIsMenuOpen }: Props) {
+  const navLinks = ['features', 'pricing', 'contact']
+
+  return (
+    <section className="text absolute inset-0 flex flex-col items-stretch bg-nav-menu/95 px-8 tracking-widest text-alternative">
+      <header className="my-10 flex justify-between">
+        <img src={logoBookmarkLight} alt="Main site logo for Bookmark" />
+        <button type="button" onClick={() => setIsMenuOpen(false)}>
+          <img className="w-4" src={closeIcon} alt="Icon to close the menu" />
+        </button>
+      </header>
+      <div className="flex-1">
+        <nav>
+          <ul className="border-t-1 border-alternative/25">
+            {navLinks.map((link) => (
+              <li
+                key={link}
+                className="border-b-1 border-alternative/25 py-4 text-center text-xl"
+              >
+                <a href={`#${link}`}>{link.toUpperCase()}</a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <button
+          className="mt-6 w-full rounded-md border-2 border-alternative py-2 text-xl font-medium"
+          type="button"
+          onClick={() => console.log('login')}
+        >
+          LOGIN
+        </button>
+      </div>
+      <footer className="center flex items-center justify-center gap-10 py-12 ">
+        <a href="#">
+          <img className="w-6" src={facebookIcon} alt="facebook icon" />
+        </a>
+        <a href="#">
+          <img className="w-6" src={twitterIcon} alt="twitter icon" />
+        </a>
+      </footer>
+    </section>
+  )
+}
+export default NavMenu
+
+type Props = {
+  setIsMenuOpen: React.Dispatch<React.SetStateAction<boolean>>
+}
