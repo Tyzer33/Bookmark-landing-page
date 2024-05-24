1c152126d4994ac490705e5b4654f535dcb13dcc
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-05-24 14:43:26 +0200

    feat: create TitleAndDescription reusable component

---
+++ src/common/TitleAndDescription.tsx
@@ -0,0 +1,38 @@
+import { useLayoutEffect, useRef, useState } from 'react'
+
+function TitleAndDescription({ children, type: Type = 'h2', title }: Props) {
+  const [lineCount, setLineCount] = useState(1)
+  const ref = useRef<HTMLDivElement>(null)
+
+  useLayoutEffect(() => {
+    function handleResize() {
+      if (!ref.current) return
+      const { lineHeight, height } = getComputedStyle(ref.current)
+      setLineCount(parseInt(height, 10) / parseInt(lineHeight, 10))
+    }
+
+    handleResize()
+
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
+  return (
+    <div
+      className={`mx-mobile space-y-4 ${lineCount > 1 ? 'space-y-4' : 'space-y-[.625rem]'}`}
+    >
+      <Type ref={ref}>{title}</Type>
+      <p>{children}</p>
+    </div>
+  )
+}
+export default TitleAndDescription
+
+type Props = {
+  children: React.ReactNode
+  type?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
+  title: string
+}
