a3bbf9c96b257a093acdf058c28970942b2d2105
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-05-24 14:42:36 +0200

    feat: create DefaultButton reusable component

---
+++ src/common/DefaultButton.tsx
@@ -0,0 +1,22 @@
+function DefaultButton({ children, className = '', theme = 'primary' }: Props) {
+  const themes = {
+    primary: 'bg-secondary text-tertiary',
+    secondary: 'bg-tertiary/5 text-secondary',
+    accent: 'bg-accent text-tertiary',
+  }
+
+  return (
+    <div
+      className={`${className} ${themes[theme]} rounded-md bg-secondary px-4 py-3 shadow-lg shadow-default`}
+    >
+      {children}
+    </div>
+  )
+}
+export default DefaultButton
+
+type Props = {
+  children: React.ReactNode
+  className?: string
+  theme?: 'primary' | 'secondary' | 'accent'
+}
