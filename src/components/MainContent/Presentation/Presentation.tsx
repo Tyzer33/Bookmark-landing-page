e89f90dcbc91cc3f8fe3ac89751b227a288cb9c3
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-05-27 10:53:43 +0200

    feat: add to Presentation its style and content

--- src/components/MainContent/Presentation/Presentation.tsx
+++ src/components/MainContent/Presentation/Presentation.tsx
@@ -7,18 +7,18 @@ function Presentation() {
     <section className="space-y-12">
       <div className="relative pb-[.625rem]">
         <img
-          className="m-auto w-[22.125rem]"
+          className="m-auto h-[15.625rem]"
           src={illustration}
           alt="Illustration of a computer screen with a browser window open"
         />
         <div className="absolute bottom-0 right-0 -z-10 h-[12.75rem] w-[19rem] rounded-l-full bg-secondary" />
       </div>
-      <div className=" flex flex-col gap-4">
+      <div className="space-y-8">
         <TitleAndDescription type="h1" title="A Simple Bookmark Manager">
           A clean and simple interface to organize your favourite websites. Open
           a new browser tab and see your sites load instantly. Try it for free.
         </TitleAndDescription>
-        <div className="mx-mobile mt-4 flex justify-between">
+        <div className="mx-mobile flex justify-between">
           <DefaultButton>Get it on Chrome</DefaultButton>
           <DefaultButton theme="secondary">Get it on Firefox</DefaultButton>
         </div>
