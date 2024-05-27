bcfac3583d0648463d1bb7cb6a2723867ff1bcef
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-05-27 17:20:54 +0200

    fix: remove tailwind utility functions

--- src/components/MainContent/Features/TabButton.tsx
+++ src/components/MainContent/Features/TabButton.tsx
@@ -1,5 +1,4 @@
 import { twJoin } from 'tailwind-merge'
-import { twAfter } from '../../../utils/tailwind'
 
 function TabButton({ handleClick, name, isSelected }: Props) {
   return (
@@ -8,9 +7,7 @@ function TabButton({ handleClick, name, isSelected }: Props) {
         'relative h-14 text-[1.0625rem]',
         isSelected ? 'text-secondary' : 'text-primary',
         isSelected &&
-          twAfter(
-            'absolute bottom-0 left-1/2 h-1 w-36 -translate-x-1/2 bg-accent content-[""]',
-          ),
+          'after:absolute after:bottom-0 after:left-1/2 after:h-1 after:w-36 after:-translate-x-1/2 after:bg-accent after:content-[""]',
       )}
       key={name}
       type="button"
--- src/utils/tailwind.ts
+++
@@ -1,9 +0,0 @@
-export function twAfter(classes: string | string[]) {
-  const cl = typeof classes === 'string' ? classes.split(' ') : classes
-  return cl.map((c) => `after:${c}`).join(' ')
-}
-
-export function twHover(classes: string | string[]) {
-  const cl = typeof classes === 'string' ? classes.split(' ') : classes
-  return cl.map((c) => `hover:${c}`).join(' ')
-}
