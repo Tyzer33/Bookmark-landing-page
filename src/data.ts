8c3561319c6e5385f89ff670940eaa983a84f240
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-05-30 11:09:50 +0200

    refactor: update data content order

--- src/data.ts
+++ src/data.ts
@@ -21,30 +21,27 @@ export const navLinks = [
   },
 ]
 
-export const questions = [
+export const tabs = [
   {
     id: 1,
-    question: 'What is Bookmark?',
-    answer:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tincidunt justo eget ultricies fringilla. Phasellus blandit ipsum quis quam ornare mattis.',
+    button: 'Simple Bookmarking',
+    image: illustrationFeaturesTab1,
+    title: 'Bookmark in one click',
+    desc: 'Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.',
   },
   {
     id: 2,
-    question: 'How can I request a new browser?',
-    answer:
-      'Vivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. Suspendisse imperdiet. Vivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. Suspendisse imperdie tVivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. Suspendisse imperdiet.',
+    button: 'Speedy Searching',
+    image: illustrationFeaturesTab2,
+    title: 'Intelligent search',
+    desc: 'Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.',
   },
   {
     id: 3,
-    question: 'Is there a mobile app?',
-    answer:
-      'Sed consectetur quam id neque fermentum accumsan. Praesent luctus vestibulum dolor, ut condimentum urna vulputate eget. Cras in ligula quis est pharetra mattis sit amet pharetra purus. Sed sollicitudin ex et ultricies bibendum.',
-  },
-  {
-    id: 4,
-    question: 'What about other Chromium browsers?',
-    answer:
-      'Integer condimentum ipsum id imperdiet finibus. Vivamus in placerat mi, at euismod dui. Aliquam vitae neque eget nisl gravida pellentesque non ut velit.',
+    button: 'Easy Sharing',
+    image: illustrationFeaturesTab3,
+    title: 'Share your bookmarks',
+    desc: 'Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.',
   },
 ]
 
@@ -69,26 +66,29 @@ export const download = [
   },
 ]
 
-export const tabs = [
+export const questions = [
   {
     id: 1,
-    button: 'Simple Bookmarking',
-    image: illustrationFeaturesTab1,
-    title: 'Bookmark in one click',
-    desc: 'Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.',
+    question: 'What is Bookmark?',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tincidunt justo eget ultricies fringilla. Phasellus blandit ipsum quis quam ornare mattis.',
   },
   {
     id: 2,
-    button: 'Speedy Searching',
-    image: illustrationFeaturesTab2,
-    title: 'Intelligent search',
-    desc: 'Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.',
+    question: 'How can I request a new browser?',
+    answer:
+      'Vivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. Suspendisse imperdiet. Vivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. Suspendisse imperdie tVivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. Suspendisse imperdiet.',
   },
   {
     id: 3,
-    button: 'Easy Sharing',
-    image: illustrationFeaturesTab3,
-    title: 'Share your bookmarks',
-    desc: 'Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.',
+    question: 'Is there a mobile app?',
+    answer:
+      'Sed consectetur quam id neque fermentum accumsan. Praesent luctus vestibulum dolor, ut condimentum urna vulputate eget. Cras in ligula quis est pharetra mattis sit amet pharetra purus. Sed sollicitudin ex et ultricies bibendum.',
+  },
+  {
+    id: 4,
+    question: 'What about other Chromium browsers?',
+    answer:
+      'Integer condimentum ipsum id imperdiet finibus. Vivamus in placerat mi, at euismod dui. Aliquam vitae neque eget nisl gravida pellentesque non ut velit.',
   },
 ]
