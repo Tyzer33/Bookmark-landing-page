55e25705e9658a6447f60752874e04868662d1ba
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-05-30 11:50:05 +0200

    feat: add responsive design to MainFooter component

--- src/components/Footer/MainFooter.tsx
+++ src/components/Footer/MainFooter.tsx
@@ -4,10 +4,16 @@ import SocialLinks from '../../common/SocialLinks'
 
 function MainFooter() {
   return (
-    <section className="flex flex-col items-center bg-tertiary text-center text-tertiary">
-      <BookmarkLogo className="mb-[2.125rem] mt-10" theme="text-light" />
-      <NavLinks ulClassName="space-y-8 text-[.9375rem]" />
-      <SocialLinks className="py-11" />
+    <section className="flex flex-col items-center bg-tertiary text-center text-tertiary lg:flex-row lg:items-center lg:px-[10.25rem] lg:py-8">
+      <BookmarkLogo
+        className="max-lg:mb-[2.125rem] max-lg:mt-10"
+        theme="text-light"
+      />
+      <NavLinks
+        navClassName="lg:ml-16 lg:mr-auto"
+        ulClassName="max-lg:space-y-8 text-[.9375rem] lg:flex"
+      />
+      <SocialLinks className="max-lg:py-11" />
     </section>
   )
 }
