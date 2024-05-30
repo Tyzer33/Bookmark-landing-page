ad9eb41f1d156fcdb421c4f6fbdd2505f62654b3
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-05-30 11:45:41 +0200

    feat: add responsive design to TitleAndDescription component

--- src/common/TitleAndDescription.tsx
+++ src/common/TitleAndDescription.tsx
@@ -12,7 +12,7 @@ function TitleAndDescription({
   return (
     <div
       className={twJoin(
-        'mx-mobile',
+        'max-w-[33.5rem] max-lg:mx-mobile lg:mx-auto lg:space-y-6',
         lineCount > 1 ? 'space-y-4' : 'space-y-[.625rem]',
       )}
     >
