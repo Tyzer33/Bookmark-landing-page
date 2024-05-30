ebcde399d429557de9aa4588a110fd2a39c0a7d8
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-05-30 11:49:51 +0200

    feat: add responsive design to Newsletter component

--- src/components/Footer/Newsletter.tsx
+++ src/components/Footer/Newsletter.tsx
@@ -2,22 +2,24 @@ import DefaultButton from '../../common/DefaultButton'
 
 function Newsletter() {
   return (
-    <section className="space-y-8 bg-secondary px-8 pb-[3.75rem] pt-[4.5rem] text-center text-tertiary">
-      <div className="space-y-3">
-        <p className="text-[.8125rem] font-medium tracking-[0.3em]">
-          35.000+ ALREADY JOINED
-        </p>
-        <h4 className="text-2xl leading-7 text-tertiary">
-          Stay up-to-date with what we&apos;re doing
-        </h4>
-      </div>
-      <div className="flex flex-col justify-center gap-4">
-        <input
-          type="email"
-          placeholder="Enter your email address"
-          className="rounded-md px-5 py-3  text-secondary"
-        />
-        <DefaultButton theme="accent">Contact Us</DefaultButton>
+    <section className="bg-secondary px-8 pb-[3.75rem] pt-[4.5rem] text-center text-tertiary lg:pb-[4.375rem] lg:pt-[4.625rem]">
+      <div className="mx-auto max-w-[27.625rem] space-y-8 lg:space-y-9">
+        <div className="space-y-3 lg:space-y-9">
+          <p className="text-[.8125rem] font-medium tracking-[0.3em] lg:tracking-[0.4em]">
+            35.000+ ALREADY JOINED
+          </p>
+          <h4 className="text-2xl leading-7 text-tertiary lg:text-[2rem] lg:leading-10">
+            Stay up-to-date with what we&apos;re doing
+          </h4>
+        </div>
+        <div className="flex w-full flex-col justify-center gap-4 lg:flex-row">
+          <input
+            type="email"
+            placeholder="Enter your email address"
+            className="flex-1 rounded-md px-5 py-3 text-secondary lg:py-4 lg:text-[.875rem] lg:leading-6"
+          />
+          <DefaultButton theme="accent">Contact Us</DefaultButton>
+        </div>
       </div>
     </section>
   )
