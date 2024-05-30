a2f3ea745a6bcfa02b69a2d48f13d6acdfb06783
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-05-30 11:38:13 +0200

    feat: add responsive design to Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,24 @@
 import BookmarkLogo from '../../common/BookmarkLogo'
+import DefaultButton from '../../common/DefaultButton'
+import NavLinks from '../../common/NavLinks'
+import useBreakpoint from '../../utils/useBreakpoint'
 import NavMenuButton from './NavMenuButton'
 
 function Header() {
+  const { isAboveLg, isBelowLg } = useBreakpoint('lg')
+
   return (
-    <header className="mx-mobile my-10 flex items-center justify-between">
-      <BookmarkLogo />
-      <NavMenuButton />
+    <header className="mx-mobile my-10 flex items-center justify-between lg:mx-[10.25rem] lg:my-12">
+      <BookmarkLogo className="lg:ml-[.375rem]" />
+      {isBelowLg && <NavMenuButton />}
+      {isAboveLg && (
+        <>
+          <NavLinks navClassName="ml-auto mr-12" ulClassName="text-secondary" />
+          <DefaultButton theme="accent" size="small">
+            LOGIN
+          </DefaultButton>
+        </>
+      )}
     </header>
   )
 }
