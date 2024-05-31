38179bf36e16197465dcd6f1e24287d5c8c222b6
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-05-31 10:07:39 +0200

    feat: upgrade responsive of TabContent component

--- src/components/MainContent/Features/TabContent.tsx
+++ src/components/MainContent/Features/TabContent.tsx
@@ -1,3 +1,4 @@
+import { twJoin } from 'tailwind-merge'
 import DefaultButton from '../../../common/DefaultButton'
 import TitleAndDescription from '../../../common/TitleAndDescription'
 import useBreakpoint from '../../../utils/useBreakpoint'
@@ -6,14 +7,14 @@ function TabContent({ image, title, desc }: Props) {
   const { isAboveLg } = useBreakpoint('lg')
 
   return (
-    <div className="flex gap-[2.625rem] max-lg:flex-col lg:gap-[7.75rem]">
-      <div className="lg:flex[1_auto] relative pb-[.125rem] lg:pb-[1.8125rem] lg:pl-[9.5rem]">
-        <img
-          className="mx-auto h-[14.75rem] lg:h-[25.375rem]"
-          src={image}
-          alt="Bookmarking"
-        />
-        <div className="absolute bottom-0 left-0 -z-10 h-[12.75rem] w-[19.375rem] rounded-r-full bg-secondary lg:h-[22rem] lg:w-[40rem]" />
+    <div className="flex items-center gap-[2.625rem] max-lg:flex-col lg:gap-[7.75rem]">
+      <div
+        className={twJoin(
+          'lg:flex[1_auto] relative -mx-[.625rem] pb-[.125rem] lg:pb-[1.8125rem] lg:pl-[9.5rem]',
+          'after:absolute after:bottom-0 after:right-[12.73%] after:-z-10 after:h-[85.71%] after:w-screen after:rounded-r-full after:bg-secondary after:lg:h-[22rem]',
+        )}
+      >
+        <img className="w-full" src={image} alt="Bookmarking" />
       </div>
       <div className="lg:mt-16 lg:max-w-[26.9375rem] lg:space-y-8 lg:text-left">
         <TitleAndDescription type="h3" title={title}>
