e9db99edc698ec5cc7e211ec38863f3e3744192e
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-05-31 10:07:49 +0200

    feat: upgrade responsive of Presentation component

--- src/components/MainContent/Presentation/Presentation.tsx
+++ src/components/MainContent/Presentation/Presentation.tsx
@@ -1,3 +1,4 @@
+import { twJoin } from 'tailwind-merge'
 import illustration from '../../../assets/illustration-hero.svg'
 import DefaultButton from '../../../common/DefaultButton'
 import TitleAndDescription from '../../../common/TitleAndDescription'
@@ -5,16 +6,20 @@ import TitleAndDescription from '../../../common/TitleAndDescription'
 function Presentation() {
   return (
     <section
-      className="mt-10 flex flex-col gap-12 lg:mt-14 lg:flex-row-reverse"
+      className="mt-10 flex flex-col items-center gap-12 lg:mt-14 lg:flex-row-reverse"
       aria-labelledby="presentationTitle"
     >
-      <div className="relative pb-[.625rem] lg:flex-[1_auto] lg:pb-7 lg:pr-[3.25rem]">
+      <div
+        className={twJoin(
+          'relative -mx-[1.3125rem] pb-[.625rem] lg:flex-[1_auto] lg:pb-7 lg:pr-[3.25rem]',
+          'after:absolute after:bottom-0 after:left-[16.71%] after:-z-10 after:h-[78.46%] after:w-screen after:rounded-l-full after:bg-secondary after:lg:h-[22rem]',
+        )}
+      >
         <img
-          className="mx-auto h-[15.625rem] lg:h-[29.125rem]"
+          className="w-full"
           src={illustration}
           alt="Illustration of a computer screen with a browser window open"
         />
-        <div className="absolute bottom-0 right-0 -z-10 h-[12.75rem] w-[19rem] rounded-l-full bg-secondary lg:h-[22rem] lg:w-[32.1875rem]" />
       </div>
       <div className="space-y-8 lg:ml-[10.25rem] lg:mt-20 lg:text-left">
         <TitleAndDescription
@@ -25,7 +30,7 @@ function Presentation() {
           A clean and simple interface to organize your favourite websites. Open
           a new browser tab and see your sites load instantly. Try it for free.
         </TitleAndDescription>
-        <div className="flex max-lg:mx-mobile max-lg:justify-between lg:gap-4">
+        <div className="flex max-lg:justify-between lg:gap-4">
           <DefaultButton>Get it on Chrome</DefaultButton>
           <DefaultButton theme="secondary">Get it on Firefox</DefaultButton>
         </div>
