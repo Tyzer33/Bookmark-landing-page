b924e4ef5c0e85bf7916d22dab7003929abba3e8
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-05-31 11:11:02 +0200

    refactor: move BackgroundShape in its own component

--- src/components/MainContent/Features/TabContent.tsx
+++ src/components/MainContent/Features/TabContent.tsx
@@ -1,21 +1,14 @@
-import { twJoin } from 'tailwind-merge'
 import DefaultButton from '../../../common/DefaultButton'
 import TitleAndDescription from '../../../common/TitleAndDescription'
 import useBreakpoint from '../../../utils/useBreakpoint'
+import BackgroundShape from '../../../common/BackgroundShape'
 
 function TabContent({ image, title, desc }: Props) {
   const { isAboveLg } = useBreakpoint('lg')
 
   return (
-    <div className="flex items-center gap-[2.625rem] max-lg:flex-col lg:gap-[7.75rem]">
-      <div
-        className={twJoin(
-          'lg:flex[1_auto] relative -mx-[.625rem] pb-[.125rem] lg:pb-[1.8125rem] lg:pl-[9.5rem]',
-          'after:absolute after:bottom-0 after:right-[12.73%] after:-z-10 after:h-[85.71%] after:w-screen after:rounded-r-full after:bg-secondary after:lg:h-[22rem]',
-        )}
-      >
-        <img className="w-full" src={image} alt="Bookmarking" />
-      </div>
+    <div className="flex items-center justify-center gap-[2.625rem] max-lg:flex-col lg:gap-[7.75rem]">
+      <BackgroundShape image={image} variant="left" />
       <div className="lg:mt-16 lg:max-w-[26.9375rem] lg:space-y-8 lg:text-left">
         <TitleAndDescription type="h3" title={title}>
           {desc}
--- src/components/MainContent/Presentation/Presentation.tsx
+++ src/components/MainContent/Presentation/Presentation.tsx
@@ -1,26 +1,15 @@
-import { twJoin } from 'tailwind-merge'
 import illustration from '../../../assets/illustration-hero.svg'
 import DefaultButton from '../../../common/DefaultButton'
 import TitleAndDescription from '../../../common/TitleAndDescription'
+import BackgroundShape from '../../../common/BackgroundShape'
 
 function Presentation() {
   return (
     <section
-      className="mt-10 flex flex-col items-center gap-12 lg:mt-14 lg:flex-row-reverse"
+      className="mt-10 flex flex-col items-center justify-center gap-12 lg:mt-14 lg:flex-row-reverse"
       aria-labelledby="presentationTitle"
     >
-      <div
-        className={twJoin(
-          'relative -mx-[1.3125rem] pb-[.625rem] lg:flex-[1_auto] lg:pb-7 lg:pr-[3.25rem]',
-          'after:absolute after:bottom-0 after:left-[16.71%] after:-z-10 after:h-[78.46%] after:w-screen after:rounded-l-full after:bg-secondary after:lg:h-[22rem]',
-        )}
-      >
-        <img
-          className="w-full"
-          src={illustration}
-          alt="Illustration of a computer screen with a browser window open"
-        />
-      </div>
+      <BackgroundShape image={illustration} variant="right" />
       <div className="space-y-8 lg:ml-[10.25rem] lg:mt-20 lg:text-left">
         <TitleAndDescription
           id="presentationTitle"
