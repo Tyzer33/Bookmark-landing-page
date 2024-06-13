707c18d6692ccb9e8daded26e4323dbe5ffff063
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-06-13 14:00:04 +0200

    feat: improve responsive of MainFooter component

--- src/components/Footer/MainFooter.tsx
+++ src/components/Footer/MainFooter.tsx
@@ -4,16 +4,18 @@ import SocialLinks from '../../common/SocialLinks'
 
 function MainFooter() {
   return (
-    <section className="flex flex-col items-center bg-tertiary text-center text-tertiary lg:flex-row lg:items-center lg:px-[10.25rem] lg:py-8">
-      <BookmarkLogo
-        className="max-lg:mb-[2.125rem] max-lg:mt-10"
-        theme="text-light"
-      />
-      <NavLinks
-        navClassName="lg:ml-16 lg:mr-auto"
-        ulClassName="max-lg:space-y-8 text-[.9375rem] lg:flex"
-      />
-      <SocialLinks className="max-lg:py-11" />
+    <section className="flex justify-center bg-tertiary text-center text-tertiary lg:px-[10.25rem] lg:py-8">
+      <div className="max-w-container flex w-full flex-col items-center lg:flex-row lg:items-center">
+        <BookmarkLogo
+          className="max-lg:mb-[2.125rem] max-lg:mt-10"
+          theme="text-light"
+        />
+        <NavLinks
+          navClassName="lg:ml-16 lg:mr-auto"
+          ulClassName="max-lg:space-y-8 text-[.9375rem] lg:flex"
+        />
+        <SocialLinks className="max-lg:py-11" />
+      </div>
     </section>
   )
 }
