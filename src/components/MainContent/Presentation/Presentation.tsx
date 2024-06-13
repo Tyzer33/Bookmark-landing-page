ad082d549c7831dd3593bf2d239e51122893d253
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-06-13 14:04:19 +0200

    feat: improve responsive of Presentation component

--- src/components/MainContent/Presentation/Presentation.tsx
+++ src/components/MainContent/Presentation/Presentation.tsx
@@ -6,11 +6,11 @@ import BackgroundShape from '../../../common/BackgroundShape'
 function Presentation() {
   return (
     <section
-      className="mt-10 flex flex-col items-center justify-center gap-12 lg:mt-14 lg:flex-row-reverse"
+      className="mt-10 flex flex-col justify-center gap-12 lg:mt-14 lg:flex-row-reverse lg:items-start lg:gap-20"
       aria-labelledby="presentationTitle"
     >
       <BackgroundShape image={illustration} variant="right" />
-      <div className="space-y-8 lg:ml-[10.25rem] lg:mt-20 lg:text-left">
+      <div className="m-auto max-w-[30rem] space-y-8 lg:mt-[clamp(0rem,-15.7055rem+24.5399vi,5rem)] lg:text-left">
         <TitleAndDescription
           id="presentationTitle"
           type="h1"
@@ -19,7 +19,7 @@ function Presentation() {
           A clean and simple interface to organize your favourite websites. Open
           a new browser tab and see your sites load instantly. Try it for free.
         </TitleAndDescription>
-        <div className="flex max-lg:justify-between lg:gap-4">
+        <div className="m-auto flex max-lg:max-w-96 max-lg:justify-between lg:gap-4">
           <DefaultButton>Get it on Chrome</DefaultButton>
           <DefaultButton theme="secondary">Get it on Firefox</DefaultButton>
         </div>
