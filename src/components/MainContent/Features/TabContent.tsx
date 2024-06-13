397de7a6b56c0cea8bf8a67cf08b1666688bfdfb
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-06-13 14:03:49 +0200

    feat: improve responsive of TabContent component

--- src/components/MainContent/Features/TabContent.tsx
+++ src/components/MainContent/Features/TabContent.tsx
@@ -7,9 +7,9 @@ function TabContent({ image, title, desc }: Props) {
   const { isAboveLg } = useBreakpoint('lg')
 
   return (
-    <div className="flex items-center justify-center gap-[2.625rem] max-lg:flex-col lg:gap-[7.75rem]">
+    <div className="flex items-start justify-center gap-[2.625rem] max-lg:flex-col lg:gap-[7.75rem]">
       <BackgroundShape image={image} variant="left" />
-      <div className="lg:mt-16 lg:max-w-[26.9375rem] lg:space-y-8 lg:text-left">
+      <div className="lg:mt-[clamp(0rem,-12.5644rem+19.6319vi,4rem)] lg:max-w-[26.9375rem] lg:space-y-8 lg:text-left">
         <TitleAndDescription type="h3" title={title}>
           {desc}
         </TitleAndDescription>
