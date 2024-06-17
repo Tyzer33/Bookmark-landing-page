c45252fc5fb60060d44fc6383006aafc9a8b504e
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-06-17 14:40:36 +0200

    feat: add hover styles for TabButton

--- src/components/MainContent/Features/TabButton.tsx
+++ src/components/MainContent/Features/TabButton.tsx
@@ -5,7 +5,7 @@ function TabButton({ handleClick, name, isSelected }: Props) {
     <button
       className={twJoin(
         'relative h-14 border-b-1 border-primary text-[1.0625rem] lg:h-20 lg:w-[15.125rem]',
-        isSelected ? 'text-secondary' : 'text-secondary/75',
+        isSelected ? 'text-secondary' : 'text-secondary/75 hover:text-accent',
         isSelected &&
           'after:absolute after:bottom-0 after:left-1/2 after:h-1 after:w-36 after:-translate-x-1/2 after:bg-accent after:content-[""] lg:after:w-full',
       )}
