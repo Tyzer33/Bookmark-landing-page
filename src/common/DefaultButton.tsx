c2a133fb30f12bd4cc0c3b49908f54e3a16336dd
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-06-17 14:39:29 +0200

    feat: add hover styles for DefaultButton

--- src/common/DefaultButton.tsx
+++ src/common/DefaultButton.tsx
@@ -1,9 +1,9 @@
 import { twMerge } from 'tailwind-merge'
 
 const themesClassnames = {
-  primary: 'bg-secondary text-tertiary',
-  secondary: 'bg-tertiary/5 text-secondary',
-  accent: 'bg-accent text-tertiary',
+  primary: 'bg-secondary text-tertiary hover:text-quaternary',
+  secondary: 'bg-tertiary/5 text-secondary hover:text-secondary',
+  accent: 'bg-accent text-tertiary hover:text-accent',
 }
 
 function DefaultButton({
@@ -17,6 +17,7 @@ function DefaultButton({
     <button
       className={twMerge(
         'rounded-md py-3 shadow-lg shadow-default',
+        'hover:bg-primary hover:outline hover:outline-2 hover:-outline-offset-2 hover:outline-current',
         size === 'small' && 'px-8 text-[.8125rem]/4 tracking-[0.11em]',
         size === 'normal' && 'px-4 text-[.9375rem]/6 lg:px-6',
         themesClassnames[theme],
