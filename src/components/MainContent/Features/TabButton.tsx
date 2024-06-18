799f9d612fbda0bf782512b26c4e8240e7e02157
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-06-18 10:57:16 +0200

    feat: add animation to TabButton

--- src/components/MainContent/Features/TabButton.tsx
+++ src/components/MainContent/Features/TabButton.tsx
@@ -1,13 +1,13 @@
-import { twJoin } from 'tailwind-merge'
+import { twMerge } from 'tailwind-merge'
 
 function TabButton({ handleClick, name, isSelected }: Props) {
   return (
     <button
-      className={twJoin(
+      className={twMerge(
         'relative h-14 border-b-1 border-primary text-[1.0625rem] transition-colors lg:h-20 lg:w-[15.125rem]',
         isSelected ? 'text-secondary' : 'text-secondary/75 hover:text-accent',
-        isSelected &&
-          'after:absolute after:bottom-0 after:left-1/2 after:h-1 after:w-36 after:-translate-x-1/2 after:bg-accent after:content-[""] lg:after:w-full',
+        'after:absolute after:bottom-0 after:left-1/2 after:h-1 after:w-36 after:-translate-x-1/2 after:scale-x-0 after:bg-accent after:opacity-0 after:transition-[transform,opacity] after:content-[""] lg:after:w-full',
+        isSelected && 'after:scale-x-100 after:opacity-100',
       )}
       key={name}
       type="button"
