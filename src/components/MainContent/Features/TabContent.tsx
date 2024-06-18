afbebecd5fce196eabc79080bb829c3092d612d5
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-06-18 11:06:49 +0200

    feat: Add slidein animation to TabContent

--- src/components/MainContent/Features/TabContent.tsx
+++ src/components/MainContent/Features/TabContent.tsx
@@ -1,3 +1,4 @@
+import { twJoin } from 'tailwind-merge'
 import DefaultButton from '../../../common/DefaultButton'
 import TitleAndDescription from '../../../common/TitleAndDescription'
 import useBreakpoint from '../../../utils/useBreakpoint'
@@ -7,7 +8,12 @@ function TabContent({ image, title, desc }: Props) {
   const { isAboveLg } = useBreakpoint('lg')
 
   return (
-    <div className="flex items-start justify-center gap-[2.625rem] max-lg:flex-col lg:gap-[7.75rem]">
+    <div
+      className={twJoin(
+        'flex items-start justify-center gap-[2.625rem] max-lg:flex-col lg:gap-[7.75rem]',
+        'animate-slidein',
+      )}
+    >
       <ImgBackgroundShape image={image} variant="left" />
       <div className="lg:mt-[clamp(0rem,-12.5644rem+19.6319vi,4rem)] lg:max-w-[26.9375rem] lg:space-y-8 lg:text-left">
         <TitleAndDescription type="h3" title={title}>
