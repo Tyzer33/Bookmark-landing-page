79f76d1faf9182c1a84699c2045c854ba5090439
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-06-19 11:10:27 +0200

    fix: Tabcontent not centered on mobile

--- src/components/MainContent/Features/TabContent.tsx
+++ src/components/MainContent/Features/TabContent.tsx
@@ -1,4 +1,3 @@
-import { twJoin } from 'tailwind-merge'
 import DefaultButton from '../../../common/DefaultButton'
 import TitleAndDescription from '../../../common/TitleAndDescription'
 import useBreakpoint from '../../../utils/useBreakpoint'
@@ -8,12 +7,7 @@ function TabContent({ image, title, desc }: Props) {
   const { isAboveLg } = useBreakpoint('lg')
 
   return (
-    <div
-      className={twJoin(
-        'flex items-start justify-center gap-[2.625rem] max-lg:flex-col lg:gap-[7.75rem]',
-        'animate-slidein',
-      )}
-    >
+    <div className="animate-slidein flex justify-center gap-[2.625rem] max-lg:flex-col max-lg:items-center lg:gap-[7.75rem]">
       <ImgBackgroundShape image={image} variant="left" />
       <div className="lg:mt-[clamp(0rem,-12.5644rem+19.6319vi,4rem)] lg:max-w-[26.9375rem] lg:space-y-8 lg:text-left">
         <TitleAndDescription type="h3" title={title}>
