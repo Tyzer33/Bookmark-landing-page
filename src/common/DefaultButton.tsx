48abbbe934b4eeaffb9f1bf1e5a5aa0eac09445a
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-06-19 15:27:31 +0200

    feat: apply hover styles to focus-visible in DefaultButton

--- src/common/DefaultButton.tsx
+++ src/common/DefaultButton.tsx
@@ -1,9 +1,11 @@
 import { twMerge } from 'tailwind-merge'
 
 const themesClassnames = {
-  primary: 'bg-secondary text-tertiary hover:text-quaternary',
-  secondary: 'bg-tertiary/5 text-secondary hover:text-secondary',
-  accent: 'bg-accent text-tertiary hover:text-accent',
+  primary:
+    'bg-secondary text-tertiary hover:text-quaternary focus-visible:text-quaternary',
+  secondary:
+    'bg-tertiary/5 text-secondary hover:text-secondary focus-visible:text-secondary',
+  accent: 'bg-accent text-tertiary hover:text-accent focus-visible:text-accent',
 }
 
 function DefaultButton({
@@ -18,6 +20,7 @@ function DefaultButton({
       className={twMerge(
         'rounded-md py-3 shadow-lg shadow-default outline outline-2 -outline-offset-2 outline-transparent transition-[outline,color,background-color]',
         'hover:bg-primary hover:outline-current',
+        'focus-visible:bg-primary focus-visible:outline-current',
         size === 'small' && 'px-8 text-[.8125rem]/4 tracking-[0.11em]',
         size === 'normal' && 'px-4 text-[.9375rem]/6 lg:px-6',
         themesClassnames[theme],
