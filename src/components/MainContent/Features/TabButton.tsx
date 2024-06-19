ec27df650199fd09675be673bac6f54df75b7b66
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2024-06-19 15:28:17 +0200

    feat: apply hover styles to focus-visible in TabButton

--- src/components/MainContent/Features/TabButton.tsx
+++ src/components/MainContent/Features/TabButton.tsx
@@ -5,7 +5,9 @@ function TabButton({ handleClick, name, isSelected }: Props) {
     <button
       className={twMerge(
         'relative h-14 border-b-1 border-primary text-[1.0625rem] transition-colors lg:h-20 lg:w-[15.125rem]',
-        isSelected ? 'text-secondary' : 'text-secondary/75 hover:text-accent',
+        isSelected
+          ? 'text-secondary'
+          : 'text-secondary/75 hover:text-accent focus-visible:text-accent',
         'after:absolute after:bottom-0 after:left-1/2 after:h-1 after:w-36 after:-translate-x-1/2 after:scale-x-0 after:bg-accent after:opacity-0 after:transition-[transform,opacity] after:content-[""] lg:after:w-full',
         isSelected && 'after:scale-x-100 after:opacity-100',
       )}
